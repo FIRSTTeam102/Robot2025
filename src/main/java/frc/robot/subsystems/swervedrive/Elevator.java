cfd8f37af8c308e0213e6e53cce01cb5ada416db
Rushali B <26rbetala@team102.org>
2025-02-01 11:12:43 -0500

    added encoder(shaft) and conversion factors and bottom limit switch

--- src/main/java/frc/robot/subsystems/swervedrive/Elevator.java
+++ src/main/java/frc/robot/subsystems/swervedrive/Elevator.java
@@ -3,6 +3,14 @@ package frc.robot.subsystems.swervedrive;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Config;
+import swervelib.parser.json.modules.ConversionFactorsJson;
+
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 
@@ -10,14 +18,22 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 public class Elevator {
     private SparkMax motor = new SparkMax( 0, MotorType.kBrushless); //change ID later
     private SparkClosedLoopController closedLoopController = motor.getClosedLoopController(); 
-    private RelativeEncoder encoder = motor.getEncoder();
-    public Elevator(){
+    // add lidar sensor (distance)
+    private SparkMaxConfig config = new SparkMaxConfig(); //encoder!!!
+   // private Encoder encoder = new Encoder(null, null); <-- might not be needed
+    private DigitalInput bottomlimitSwitch = new DigitalInput(1); //change channel later
 
+    public Elevator(){
+        config.encoder
+        .positionConversionFactor(1000)
+        .velocityConversionFactor(1000); //change factors later
     }
+    
 
 
 float height = 0; 
 public void moveManual (){
+    //setMotorSpeed(XboxController.getRawAxis(2));
 
 }
 public void moveUp (float height) {
