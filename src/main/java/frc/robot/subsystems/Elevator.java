8943264864f31d732784757101f0af59da6db1c6
Rushali B <26rbetala@team102.org>
2025-02-15 11:28:19 -0500

    fixed P value and FF value

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -81,15 +81,15 @@ public class Elevator extends SubsystemBase {
         .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
         // Set PID values for position control. We don't need to pass a closed loop
         // slot, as it will default to slot 0.
-        .p(0.1)
+        .p(0.25)
         .i(0)
         .d(0)
         .outputRange(ElevatorConstants.kMinOutput, ElevatorConstants.kMaxOutput)
         // Set PID values for velocity control in slot 1
-        .p(.1, ClosedLoopSlot.kSlot1)
+        .p(.25, ClosedLoopSlot.kSlot1)
         .i(0, ClosedLoopSlot.kSlot1)
         .d(0, ClosedLoopSlot.kSlot1)
-        .velocityFF(0.1, ClosedLoopSlot.kSlot1)
+        .velocityFF(0.01056, ClosedLoopSlot.kSlot1)
         .outputRange(-1, 1, ClosedLoopSlot.kSlot1);
 
     /*
